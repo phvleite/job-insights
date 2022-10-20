dc89af7fa70c20a45db63e08716c2f6d28a8b0c0
phvleite <phvleite@gmail.com>
2022-10-20 20:27:31 -0300

    feat: criada função read(path) para ler os arquivos csv e retorna uma lista de dict deste

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,16 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    if not path.endswith('.csv'):
+        raise ValueError('Formato inválido!')
+    with open(path, encoding='utf-8') as file:
+        read_file = csv.reader(file, delimiter=',')
+        header, *data = read_file
+        result_list = []
+        for row in data:
+            dict_result = {}
+            for ind, elem in enumerate(row):
+                dict_result[header[ind]] = elem
+            result_list.append(dict_result)
+
+    return result_list
