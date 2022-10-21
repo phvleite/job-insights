4fd2e6e7ce55c0bfca340731e84d8aa7ab056d74
phvleite <phvleite@gmail.com>
2022-10-20 22:07:41 -0300

    feat: criada a função 'get_unique_job_types()' em src/insights.py

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,15 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    result = read(path)
+    unique_jobs = set()
+
+    for job_type in result:
+        unique_jobs.add(job_type['job_type'])
+
+    unique_list = [job for job in unique_jobs]
+
+    return unique_list
 
 
 def filter_by_job_type(jobs, job_type):
