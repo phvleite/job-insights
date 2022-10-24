286433b8ae769e2d07b5a2f3b9a9f06e9abc9f8f
phvleite <phvleite@gmail.com>
2022-10-23 22:22:31 -0300

    feat: implementado o teste para a função 'read_brazilian_file()' em test_brazilian_jobs.py

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    TAGS = ['title', 'salary', 'type']
+    result = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    for job in result:
+        assert TAGS[0] in job
+        assert TAGS[1] in job
+        assert TAGS[2] in job
